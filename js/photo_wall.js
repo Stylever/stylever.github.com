f7240bc38d4a7dfcd096cfa2cb30fc7bbe91e556
a39955851 <lidongyang23@gmail.com>
2015-08-30 10:50:05 +0800

    photo wall

--- js/photo_wall.js
+++ js/photo_wall.js
@@ -25,27 +25,20 @@ require(['template', 'jquery'], function (template) {
 		selectAction: function () { // event binding
 			// show big photos
 			$(".row").on("click", ".photo", function () {
-				// var arrIndex = $(this).attr("arr-index"), // eg: imgsArr index 1-2 
-				// 	arr = arrIndex.split("-"); // [1, 2]
 
 				photoWall.appendOriginPhoto($(this).attr("arr-index"));
-				// gallery.num = -arr[1];
-				// gallery.appendSpinImg(arr[0]);
-				// gallery.spinImg(arr[0])
-				// gallery.changeSpin(-arr[1]);
-				$(".popup").show();
-				$(".photo-wall").hide();
 
-				photoWall.autoHeight();
+				$(".popup-photo-wall").show();
+				$(".photo-wall").hide();
 			});
 
 			// close big photo
-			$(".popup").on("click", ".close", function () {
+			$(".popup-photo-wall").on("click", ".close", function () {
 				$(".photo-wall").show();
-				$(".popup").hide();
+				$(".popup-photo-wall").hide();
 			});
 			// next photo
-			$(".popup").on("click", ".next", function () {
+			$(".popup-photo-wall").on("click", ".next", function () {
 				var arrIndex = $(".origin-img").attr("arr-index"),
 					arr = arrIndex.split("-"),
 					index = $(".origin-img img").attr("index");
@@ -58,11 +51,10 @@ require(['template', 'jquery'], function (template) {
 					} else { // the last photo
 						$(".origin-img img").attr("src", "../images/" + photoArr[0]).attr("index", "0");
 					}
-					photoWall.autoHeight();
 				}
 			});
 			// prev photo
-			$(".popup").on("click", ".prev", function () {
+			$(".popup-photo-wall").on("click", ".prev", function () {
 				var arrIndex = $(".origin-img").attr("arr-index"),
 					arr = arrIndex.split("-"),
 					index = $(".origin-img img").attr("index");
@@ -78,12 +70,6 @@ require(['template', 'jquery'], function (template) {
 					photoWall.autoHeight();
 				}
 			});
-		},
-		autoHeight: function () { // caculate the height of orign-img element
-			setTimeout(function () { // add height
-				var height = $(".origin-img").outerHeight(true);
-				$(".popup").css('height',  height + 'px');
-			}, 1000);
 		},
 		appendOriginPhoto: function (arrIndex) { // show origin photo
 			var arr = arrIndex.split("-"),
