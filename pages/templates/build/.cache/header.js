36e4ecd44a1ecd727e89844e00ffe87360b2e369
a39955851 <lidongyang23@gmail.com>
2016-04-04 20:35:26 +0800

    change blog site

--- pages/templates/build/.cache/header.js
+++ pages/templates/build/.cache/header.js
@@ -1,9 +1,9 @@
-/*TMODJS:{"version":1,"md5":"e5605d4e84b75dc6e946ae6b5a65afb5"}*/
+/*TMODJS:{"version":2,"md5":"f99b2a5416ecf7240e5484ab207a0fa1"}*/
 template('header',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,language=$data.language,$out='';$out+='<nav class="navbar navbar-style"> <div class="container-fluid"> <div class="navbar-header"> <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="/">Style</a> </div> <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> ';
 if(language == 0){
-$out+=' <ul class="nav navbar-nav"> <li class="active"> <a href="/">Home <span class="sr-only">(current)</span> </a> </li> <li> <a href="/pages/project.html">Projects</a> </li> <li> <a href="/pages/blog.html">Blog</a> </li> <li> <a href="/pages/about.html">About</a> </li> </ul> ';
+$out+=' <ul class="nav navbar-nav"> <li class="active"> <a href="/">Home <span class="sr-only">(current)</span> </a> </li> <li> <a href="/pages/project.html">Projects</a> </li> <li> <a href="/web-cookbook">Blog</a> </li> <li> <a href="/pages/about.html">About</a> </li> </ul> ';
 }else{
 $out+=' <ul class="nav navbar-nav"> <li class="active"> <a href="/">首页 <span class="sr-only">(current)</span> </a> </li> <li> <a href="/pages/project.html">项目</a> </li> <li> <a href="/pages/blog.html">博客</a> </li> <li> <a href="/pages/about.html">关于</a> </li> </ul> ';
 }
